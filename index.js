3b0af50fe750ee4f6e00ce6a6dfaa6966d93225d
William Hwang <willhwang90@gmail.com>
2022-04-27 12:43:44 -0400

    commit tree 793508ddb024ba1045c72243dd5e4c3bd3a2e48d

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+const destructivelyAppendCat = (name) => cats.push(name);
+
+const destructivelyPrependCat = (name) => cats.unshift(name);
+
+const destructivelyRemoveLastCat = () => cats.pop();
+
+const destructivelyRemoveFirstCat = () => cats.shift();
+
+const appendCat = (name) => [...cats, name];
+
+const prependCat = (name) => [name, ...cats];
+
+const removeLastCat = () => [...cats.slice(0,-1)]
+
+const removeFirstCat = () => [...cats.slice(1)]
